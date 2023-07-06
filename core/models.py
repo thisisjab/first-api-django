c041f4f57ae05534fd165c0caab629e043a4cbc6
thisisjab <heythisisjab@gmail.com>
2023-07-06 10:29:07 +0330

    Chnage AbstractUser to AbstractBaseUser

--- core/models.py
+++ core/models.py
@@ -1,12 +1,15 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+from django.utils import timezone
 
 from .managers import UserManager
 
 
-class User(AbstractUser):
-    username = None
+class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField("email address", unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
