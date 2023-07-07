78e60103d1b768877e182620c3a57b9c9e323213
thisisjab <heythisisjab@gmail.com>
2023-07-07 19:08:50 +0330

    Create portal models and do migrations

--- portal/models.py
+++ portal/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, blank=False, null=False, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Company(models.Model):
+    title = models.CharField(max_length=255, blank=False, null=False, unique=True)
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Job(models.Model):
+    title = models.CharField(max_length=255, blank=False, null=False)
+    description = models.TextField(blank=False, null=False)
+    category = models.ManyToManyField(Category)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.title
