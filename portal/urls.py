5f33136f3092c186763dec074668d3b0801ae47a
thisisjab <heythisisjab@gmail.com>
2023-07-08 10:41:43 +0330

    Register Job model views

--- portal/urls.py
+++ portal/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('jobs/', views.jobs_list, name='jobs_list'),
+    path('jobs/', views.JobsListView.as_view(), name='jobs_list'),
+    path('jobs/<int:id>/', views.JobDetailView.as_view(), name='job_detail'),
 ]
