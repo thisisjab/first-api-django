462b592f94c64e8cf86d3a1f6731caec9733231c
thisisjab <heythisisjab@gmail.com>
2023-07-08 15:00:08 +0330

    Create Review model and its migrations in portal app

--- portal/models.py
+++ portal/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 
 class Category(models.Model):
@@ -23,3 +24,12 @@ class Job(models.Model):
 
     def __str__(self) -> str:
         return self.title
+
+
+class Review(models.Model):
+    body = models.CharField(max_length=255, blank=False, null=False)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.body
