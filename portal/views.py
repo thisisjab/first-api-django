e87d4cd0306a2912794842d777b7a69bb12e38ea
thisisjab <heythisisjab@gmail.com>
2023-07-08 12:16:46 +0330

    Use genereic view for CategoryListView

--- portal/views.py
+++ portal/views.py
@@ -19,17 +19,9 @@ class JobDetailView(RetrieveUpdateDestroyAPIView):
     serializer_class = serializers.JobSerializer
 
 
-class CategoryListView(APIView):
-    def get(self, request):
-        categories = models.Category.objects.all()
-        serializer = serializers.CategorySerializer(categories, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    def post(self, request):
-        serializer = serializers.CategorySerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+class CategoryListView(ListCreateAPIView):
+    queryset = models.Category.objects.all()
+    serializer_class = serializers.CategorySerializer
 
 
 class CategoryDetailView(APIView):
