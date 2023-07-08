f76848ed55c2b81785206a28513819a892d0a148
thisisjab <heythisisjab@gmail.com>
2023-07-08 14:01:00 +0330

    Create and register routers for portal models' view sets

--- portal/urls.py
+++ portal/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
+from django.urls import include
+from rest_framework.routers import SimpleRouter
 
 from . import views
 
+router = SimpleRouter()
+router.register('jobs', views.JobViewSet)
+router.register('categories', views.CategoryViewSet)
+router.register('companies', views.CompnayViewSet)
+
 urlpatterns = [
-    path('jobs/', views.JobsListView.as_view(), name='jobs_list'),
-    path('jobs/<int:pk>/', views.JobDetailView.as_view(), name='job_detail'),
-    path('categories/', views.CategoryListView.as_view(), name='category_list'),
-    path('categories/<int:pk>/', views.CategoryDetailView.as_view(), name='category_detail'),
-    path('companies/', views.CompanyListView.as_view(), name='company_view'),
-    path('companies/<int:pk>/', views.CompanyDetailView.as_view(), name='company_detail'),
+    path('', include(router.urls))
 ]
