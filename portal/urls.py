31452361b2fc0f4e5507b7e3183e41fb83de5ae9
thisisjab <heythisisjab@gmail.com>
2023-07-08 11:30:13 +0330

    Register urls for Company API views

--- portal/urls.py
+++ portal/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('jobs/<int:id>/', views.JobDetailView.as_view(), name='job_detail'),
     path('categories/', views.CategoryListView.as_view(), name='category_list'),
     path('categories/<int:id>/', views.CategoryDetailView.as_view(), name='category_detail'),
+    path('companies/', views.CompanyListView.as_view(), name='company_view'),
+    path('companies/<int:id>/', views.CompanyDetailView.as_view(), name='company_detail'),
 ]
