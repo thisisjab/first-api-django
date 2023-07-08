202ae1de123a54cd5692ea332e96c25ab7d36360
thisisjab <heythisisjab@gmail.com>
2023-07-08 12:18:50 +0330

    Use genereic view for CompanyDetailView

--- portal/views.py
+++ portal/views.py
@@ -35,20 +35,7 @@ class CompanyListView(ListCreateAPIView):
     serializer_class = serializers.CompanySerializer
     
 
-class CompanyDetailView(APIView):
-    def get(self, request, id):
-        company = get_object_or_404(models.Company, id=id)
-        serializer = serializers.CompanySerializer(company)
-        return Response(serializer.data, status.HTTP_200_OK)
-    
-    def put(self, request, id):
-        company = get_object_or_404(models.Company, id=id)
-        serializer = serializers.CompanySerializer(company, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-    
-    def delete(self, request, id):
-        company = get_object_or_404(models.Company, id=id)
-        company.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class CompanyDetailView(RetrieveUpdateDestroyAPIView):
+    lookup_field = 'id'
+    queryset = models.Company.objects.all()
+    serializer_class = serializers.CompanySerializer
