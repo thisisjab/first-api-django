8b0e2076505375d93ef76e520329bf47cbb035ef
thisisjab <heythisisjab@gmail.com>
2023-07-08 22:25:38 +0330

    Install djoser

--- job_portal/settings.py
+++ job_portal/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'djoser',
     'django_filters',
     'core.apps.CoreConfig',
     'portal.apps.PortalConfig',
