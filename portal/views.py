14c943830e1d82359b7385e8e1f527e1276171fa
thisisjab <heythisisjab@gmail.com>
2023-07-08 11:23:56 +0330

    Create API views for Category model

--- portal/views.py
+++ portal/views.py
@@ -37,3 +37,35 @@ class JobDetailView(APIView):
         job = get_object_or_404(models.Job, id=id)
         job.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class CategoryListView(APIView):
+    def get(self, request):
+        categories = models.Category.objects.all()
+        serializer = serializers.CategorySerializer(categories, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = serializers.CategorySerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class CategoryDetailView(APIView):
+    def get(self, request, id):
+        category = get_object_or_404(models.Category, id=id)
+        serializer = serializers.CategorySerializer(category)
+        return Response(serializer.data, status.HTTP_200_OK)
+    
+    def put(self, request, id):
+        category = get_object_or_404(models.Category, id=id)
+        serializer = serializers.CategorySerializer(category, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    
+    def delete(self, request, id):
+        category = get_object_or_404(models.Category, id=id)
+        category.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
