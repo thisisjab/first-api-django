b39a6ff2e043318d0d6367270fc032e84c992ad3
thisisjab <heythisisjab@gmail.com>
2023-07-08 18:11:56 +0330

    Change routers to nested routers and register Review model view set

--- portal/urls.py
+++ portal/urls.py
@@ -1,14 +1,18 @@
 from django.urls import path
 from django.urls import include
-from rest_framework.routers import SimpleRouter
+from rest_framework_nested import routers
 
 from . import views
 
-router = SimpleRouter()
+router = routers.DefaultRouter()
 router.register('jobs', views.JobViewSet)
 router.register('categories', views.CategoryViewSet)
 router.register('companies', views.CompnayViewSet)
 
+companies_router = routers.NestedDefaultRouter(router, 'companies', lookup='company')
+companies_router.register('reviews', views.ReviewViewSet, basename='company-reviews')
+
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('', include(companies_router.urls)),
 ]
