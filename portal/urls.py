3e368efdb3de35f77e1fbd25258e3c25a44b729e
thisisjab <heythisisjab@gmail.com>
2023-07-08 11:24:16 +0330

    Register urls for Category API views

--- portal/urls.py
+++ portal/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('jobs/', views.JobsListView.as_view(), name='jobs_list'),
     path('jobs/<int:id>/', views.JobDetailView.as_view(), name='job_detail'),
+    path('categories/', views.CategoryListView.as_view(), name='category_list'),
+    path('categories/<int:id>/', views.CategoryDetailView.as_view(), name='category_detail'),
 ]
