8541866ea2ab9cf2dab0f5e7803b22dcdf156449
thisisjab <heythisisjab@gmail.com>
2023-07-09 11:35:50 +0330

    Register djoser urls

--- job_portal/urls.py
+++ job_portal/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('portal/', include('portal.urls')),
+    path('auth/', include('djoser.urls')),
     path('__debug__/', include('debug_toolbar.urls')),
 ]
