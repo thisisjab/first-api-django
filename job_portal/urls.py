dc8efcb02b1c43d50651961eaa98a6786353d479
thisisjab <heythisisjab@gmail.com>
2023-07-09 11:54:23 +0330

    Register djoser jwt urls

--- job_portal/urls.py
+++ job_portal/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('portal/', include('portal.urls')),
     path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
     path('__debug__/', include('debug_toolbar.urls')),
 ]
